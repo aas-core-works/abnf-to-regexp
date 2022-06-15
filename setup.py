ad463807e6ecb4cf0d7de94dec569e1e0e63ca8a
Marko Ristin <marko.ristin@gmail.com>
2022-06-15 17:04:36 +0200

    Add support for Python 3.9 and 3.10
    We include the Python versions 3.9 and 3.10 in the continous
    integration.

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     license="License :: OSI Approved :: MIT License",
     keywords="abnf backus-naur convert regular expressions",
