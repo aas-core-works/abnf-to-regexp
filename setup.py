03ae617b61e7799392f74d5eab9529f8871441d2
Marko Ristin <marko.ristin@gmail.com>
2024-03-22 09:55:40 +0100

    Discontinue Python 3.7 and include 3.11
    We discontinue Python 3.7 as we can not run `black` on it anymore.
    
    We also include Python 3.11 in CI to support a more modern version.

--- setup.py
+++ setup.py
@@ -31,10 +31,10 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     license="License :: OSI Approved :: MIT License",
     keywords="abnf backus-naur convert regular expressions",
