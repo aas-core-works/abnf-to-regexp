8ff5177b1c540a18de0bd5885b987cc9b4041eac
Marko Ristin-Kaufmann <marko@ristin.ch>
2025-11-08 18:58:32 +0100

    Release 1.2.0
    * Support Python 3.12 and 3.13 (#55)
    * Improve regexp generation for ``#`` (#50)
    * Remove spurious CRLF warning (#48)
    * Improve python-nested regex output for repetition (#45)
    * Fix default for format argument (#42)
    * Constrain abnf dependency to 2.2.0 (#43)
    
    This is a minor version which mainly improves on the compatibility of
    generated regular expressions so that they can be (re-)used across
    more platforms.

--- abnf_to_regexp/__init__.py
+++ abnf_to_regexp/__init__.py
@@ -1,7 +1,7 @@
 """Convert ABNF grammars to Python regular expressions."""
 
-# Do not forget to update in setup.py!
-__version__ = "1.1.3"
+# Do not forget to update in pyproject.toml!
+__version__ = "1.2.0"
 __author__ = "Marko Ristin"
 __license__ = "License :: OSI Approved :: MIT License"
 __status__ = "Production/Stable"
